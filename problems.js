fd5ab8772f3a1035e2e2909692a505b21ab92ec0
mrgazanfarli <mr.gazanfarli@gmail.com>
2020-11-22 14:01:35 +0400

    added a new function

--- problems.js
+++ problems.js
@@ -25,3 +25,29 @@ function getSumIndexes(arrayOfNumbers, target) {
 }
 
 // console.log(getSumIndexes([11, 15, 2, 7], 9));
+
+// Remove element
+
+/*
+The function takes an array and an element, and removes each reference of the element from the array.
+Then, returns the length of new array
+*/
+
+// [1, 2, 3, 3, 4, 6, 3, 7]
+function removeValueFromArray(array, value) {
+    let j = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== value) {
+            array[j] = array[i];
+            j++;
+        }
+    }
+
+    return j;
+}
+
+console.log(removeValueFromArray([1, 2, 3, 3, 4, 6, 3, 7], 3));
+
+
+
