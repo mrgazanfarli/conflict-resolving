65720ccd031b7c257ecdb5fa99b9ee4484164758
mrgazanfarli <mr.gazanfarli@gmail.com>
2020-11-22 11:50:58 +0400

    added objects

---
+++ object.js
@@ -0,0 +1,23 @@
+console.clear();
+
+const elvin = {
+    name: 'Elvin',
+    age: 37,
+    gender: 'male',
+}
+
+const users = [
+    {
+        name: 'Anar',
+        gender: 'male'
+    },
+    {
+        name: 'Ismayil',
+        gender: 'male'
+    },
+    {
+        name: 'Aytan',
+        gender: 'female'
+    },
+];
+
